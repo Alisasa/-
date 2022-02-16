0b2997415d4af688b7fd67fe20b5d1a832d4d897
Alisasa <77996954+Alisasa@users.noreply.github.com>
2022-02-16 11:38:31 -0800

    Update DFS.py

--- 九章算法班/Week4 DFS深搜/DFS.py
+++ 九章算法班/Week4 DFS深搜/DFS.py
@@ -16,6 +16,10 @@ combination和permutation template difference:
 
 问题：
 1.特殊情况处理时 not，is None，len == 0的区别与用法场景
+if a会首先去调用a的__nonzero__()去判断a是否为空，并返回True/False，若一个对象没有定义__nonzero__()，就去调用它的__len__()来进行判断（这里返回值为0代表空），若某一对象没有定义以上两种方法，则if a的结果永远为True
+ a is None 就只是判断 a 是不是为 None，只有 a 是 None 才为 True，而其他的空字符串("")，空数组([])则不满足
+ https://www.freecodecamp.org/news/truthy-and-falsy-values-in-python/ 这个是  is None
+ not 基于对象在转成 Boolean 值后是 True 或 False 后再做一次取反操作
 '''
 '''1.组合类'''
 #lintcode 17. subset 没有重复数字
@@ -158,6 +162,35 @@ def permute(self, nums: List[int]) -> List[List[int]]:
             
 #lintcode 10
 '''去重逻辑：如果前面的b还没有选掉，就不能选择后面的b。否则，会造成重复'''
+def stringPermutation2(self, str):
+        # write your code here
+        if str is None:
+            return 
+        #给str排序 为了使重复元素相邻
+        sorted_str = sorted(list(str))
+        permutations = []
+        visited = [False] * len(sorted_str)
+        self.dfs(sorted_str, visited, [], permutations)
+        return permutations
+    
+    def dfs(self, chars, visited, permutation, permutations):
+        # 递归的出口
+        if len(chars) == len(permutation):
+            permutations.append(''.join(permutation))
+            return 
+        for i in range(len(chars)):
+            #if visited[i] is true, means we've already visited it
+            if visited[i]:
+                continue
+            #if it is same as previous one and we didn't used previous one yet, continue to avoid duplicate
+            if i > 0 and chars[i - 1] == chars[i] and not visited[i-1]:
+                continue
+            visited[i] = True
+            permutation.append(chars[i]) 
+            self.dfs(chars, visited, permutation, permutations)
+            permutation.pop()
+            visited[i] = False
+
 
 
 
